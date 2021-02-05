7adb740cb552320d801ebad1dbdd4dee89359854
Dantum <dandeltumaag@github.com>
2021-02-06 01:36:31 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CVAO PET OWNERS</title>
+</head>
+<body>
+  <h1>OWNER DETAILS</h1>
+  <form action="post">
+    <div>
+      <label for="txt_ownerName">Name</label>
+      <input type="text" name="txt_ownerName" id="txt_ownerName">
+    </div>
+    <div>
+      <label for="txt_address">Address</label>
+      <input type="text" name="txt_address" id="txt_address">
+    </div>
+    <div>
+      <label for="txt_contact">Contact</label>
+      <input type="text" name="txt_contact" id="txt_contact">
+    </div>
+  </form>
+  <h1>PET DETAILS</h1>
+</body>
+</html>
